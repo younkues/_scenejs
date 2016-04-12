d8c9b6db8134e90d8d66bc21bafff34a53f91c6a
Daybrush <itoolsg@gmail.com>
2016-04-13 01:49:18 +0900

    gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-gulp.src('public/src/js/*.js')
-	.pipe(stripDebug())
-	.pipe(uglify())
-	.pipe(concat('script.js'))
-	.pipe(gulp.dest('public/dist/js'));
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+
+
+gulp.task('combine-js', function () {
+	return gulp.src([
+	"constant.js",
+	"pre.js",
+	"Scene.js",
+])
+	.pipe(concat('Scene.all.js'))
+	.pipe(gulp.dest('./'));
+});
+gulp.task('default', ['combine-js']);
