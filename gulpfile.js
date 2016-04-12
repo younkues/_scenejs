07030ccee8963af21e0cea22c9f82eb909dfb7a7
Daybrush <itoolsg@gmail.com>
2016-04-13 02:04:01 +0900

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,17 @@ var uglify = require('gulp-uglify');
 
 gulp.task('combine-js', function () {
 	return gulp.src([
-	"constant.js",
-	"pre.js",
-	"Scene.js",
-	"SceneItem.js",
-	"Frame.js",
-	"Curve.js",
-	"TimingFunction.js",
-	"PropertyObject.js",
-	"PropertyFunction.js",
-	"Util.js",
-	"Color.js",	
+	"./src/constant.js",
+	"./src/pre.js",
+	"./src/Scene.js",
+	"./src/SceneItem.js",
+	"./src/Frame.js",
+	"./src/Curve.js",
+	"./src/TimingFunction.js",
+	"./src/PropertyObject.js",
+	"./src/PropertyFunction.js",
+	"./src/Util.js",
+	"./src/Color.js",	
 ])
 	.pipe(concat('Scene.all.js'))
 	.pipe(gulp.dest('./'));
