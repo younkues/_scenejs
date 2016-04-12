c12cea738969204504e06b343f8736c1ffe5dc42
Daybrush <itoolsg@gmail.com>
2016-04-13 01:50:21 +0900

    gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,19 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
-
-
 gulp.task('combine-js', function () {
 	return gulp.src([
 	"constant.js",
 	"pre.js",
 	"Scene.js",
+	"SceneItem.js",
+	"Frame.js",
+	"Curve.js",
+	"TimingFunction.js",
+	"PropertyObject.js",
+	"PropertyFunction.js",
+	"Util.js",
+	"Color.js",	
 ])
 	.pipe(concat('Scene.all.js'))
 	.pipe(gulp.dest('./'));
