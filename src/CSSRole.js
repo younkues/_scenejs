010d6c29f457e498b356dc8161ddc75f763fc8d1
Daybrush <itoolsg@gmail.com>
2016-06-19 12:10:49 +0900

    fixed error

---
+++ src/CSSRole.js
@@ -0,0 +1,95 @@
+Scene.addRole("property", "properties");
+Scene.addRole("transform", "transforms");
+Scene.addRole("filter", "filters");
+
+var _synchronize = sceneItemPrototype.synchronize;
+sceneItemPrototype.synchronize = (function(_synchronize) {
+	return function(time) {
+	var time = _synchronize.call(this, time);
+	if(time === false)
+		return false;
+	var frame = this.getNowFrame(time);
+
+	if(this.animateFunction)
+		this.animateFunction(time);
+		
+		
+	var cssText = frame.getCSSText();
+
+	
+		
+	if(!this.element)
+		return false;
+
+	this.element.style.cssText = cssText;
+	
+	return true;
+};})(_synchronize);
+
+framePrototype.getCSSObject = function() {
+	var transforms = this.transforms, filters = this.filters, properties = this.properties;
+	var value;
+	var cssObject = {}, cssTransform = "", cssFilter = "";
+	/*transform css*/
+	for(var transformName in transforms) {
+		value = transforms[transformName];
+		try {
+			if(value instanceof Object)
+				value = value.toValue();
+			if(typeof value === "undefined")
+				continue;
+			cssTransform += transformName + "(" + value + ")";
+			cssTransform += " ";
+		} catch(e) {}
+	}
+	cssObject["transform"] = cssTransform;
+	/*filter css*/
+	for(var filterName in filters) {
+		value = filters[filterName];
+		try {
+			if(value instanceof Object)
+				value = value.toValue();
+			if(typeof value === "undefined")
+				continue;
+			cssFilter += filterName + "(" + value + ")";
+			cssFilter += " ";
+		} catch(e){}
+	}
+	cssObject["filter"] = cssFilter;
+	/*property css*/
+	for(var propertyName in properties) {
+		value = properties[propertyName];
+		try {
+			if(value instanceof Object)
+				value = value.toValue();
+			if(typeof value === "undefined")
+				continue;
+			cssObject[propertyName] = value;
+		} catch(e) {}
+	}
+	return cssObject;
+}
+/*
+	크로스 브라우징 접두사를 추가시켜준다.
+*/
+var convertCrossBrowserCSSObject = function(cssObject, property) {
+	cssObject["-moz-" + property] =
+	cssObject["-ms-" + property] =
+	cssObject["-o-" + property] =
+	cssObject["-webkit-" + property] = cssObject[property];
+}
+/*
+	CSSObject를 cssText로 바꿔준다.
+*/
+framePrototype.getCSSText = function() {
+	var cssObject = this.getCSSObject();
+	var cssText = "", value, property;
+	convertCrossBrowserCSSObject(cssObject, "transform");
+	convertCrossBrowserCSSObject(cssObject, "filter");
+	
+	for(property in cssObject) {
+		cssText += property + ":" + cssObject[property] +";";
+	}
+
+	return cssText;
+}
