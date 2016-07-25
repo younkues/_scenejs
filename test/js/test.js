7174b98d5a85ad97fe3f5065fa56e0a518c7b278
Daybrush <itoolsg@gmail.com>
2016-07-26 04:02:32 +0900

    add QUnitTest

--- test/js/test.js
+++ test/js/test.js
@@ -1,5 +1,5 @@
 var scene = new Scene();
-test( "Scene Basic Test", function( assert ) {
+QUnit.test( "Scene Basic Test", function( assert ) {
 	scene.load({
 		"test1" : {
 			0 : {"t1":"1px", transform:{"t2": "30deg"}, "t3":"rgba(0, 0, 0, 0)"},
@@ -10,4 +10,8 @@ test( "Scene Basic Test", function( assert ) {
 			1 : {"t1":"5px", transform:{"t2": "40deg"}, "t3":"rgba(0, 0, 0, 0)"},
 		},
 	});
+	
+	assert.equal(scene.getItem("test1").getProperty(0, "t1"), "1px", "getProperty value test1");
+	assert.equal(scene.getItem("test1").getProperty(1, "t1"), "5px", "getProperty value test2");
+	assert.equal(scene.getItem("test1").getProperty(1, "t3").type, "color", "Color Property Object");
 });
